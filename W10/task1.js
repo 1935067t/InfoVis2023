898576acc020edd4e6240faf9704f817f7464c65
Baba Kaisei <batanq5axela@gmail.com>
2023-12-15 22:51:03 +0900

    create W10

---
+++ W10/task1.js
@@ -0,0 +1,87 @@
+d3.csv("https://1935067t.github.io/InfoVis2023/W10/w10_task1.csv")
+    .then( data => {
+        data.forEach( d => { d.x = +d.x; d.y = +d.y; });
+
+        var config = {
+            parent: '#drawing_region',
+            width: 256,
+            height: 128,
+            margin: {top:10, right:10, bottom:20, left:60}
+        };
+
+        const bar_chart = new BarChart( config, data );
+        bar_chart.update();
+    })
+    .catch( error => {
+        console.log( error );
+    });
+
+class BarChart {
+
+    constructor( config, data ) {
+        this.config = {
+            parent: config.parent,
+            width: config.width || 256,
+            height: config.height || 256,
+            margin: config.margin || {top:10, right:10, bottom:20, left:60}
+        }
+        this.data = data;
+        this.init();
+    }
+
+    init() {
+        let self = this;
+
+        self.svg = d3.select( self.config.parent )
+            .attr('width', self.config.width)
+            .attr('height', self.config.height);
+
+        self.chart = self.svg.append('g')
+            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+
+        self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
+        self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+
+        self.xscale = d3.scaleLinear()
+            .domain([0, d3.max(self.data, d => d.length)])
+            .range([0, self.inner_width]);
+      
+        self.yscale = d3.scaleBand()
+            .domain(self.data.map(d => d.label))
+            .range([0, self.inner_height])
+            .paddingInner(0.1);
+
+
+        self.xaxis = d3.axisBottom( self.xscale )
+            .ticks(7)
+            .tickSizeOuter(0);
+      
+        self.yaxis = d3.axisLeft( self.yscale )
+            .tickSizeOuter(0);
+
+        self.xaxis_group = self.chart.append('g')
+        .attr('transform', `translate(0, ${self.inner_height})`)
+        .call( self.xaxis );
+    
+        self.yaxis_group = self.chart.append('g')
+        .call( self.yaxis );
+
+    }
+
+    update() {
+        let self = this;
+
+        self.render();
+    }
+
+    render() {
+        let self = this;
+
+        self.chart.selectAll("rect").data(self.data).enter()
+        .append("rect")
+        .attr("x", 0)
+        .attr("y", d => self.yscale(d.label))
+        .attr("width", d => self.xscale(d.length))
+        .attr("height", self.yscale.bandwidth());
+    }
+}
